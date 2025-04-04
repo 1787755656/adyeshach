b834e5db3e055f8961b467bbd729ad776857b52e
xwdj <40928432+1787755656@users.noreply.github.com>
2025-04-05 02:01:47 +0800

    Update build.gradle.kts

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -52,25 +52,9 @@ tasks {
 publishing {
     repositories {
         mavenLocal()
-maven {
-        name = "Aliyun"
-        url = 'https://maven.aliyun.com/repository/public'
-    }
-    
-    // 阿里云Spring仓库
-    maven {
-        name = "Aliyun-Spring"
-        url = 'https://maven.aliyun.com/repository/spring'
-    }
-    
-    // 阿里云Google仓库
-    maven {
-        name = "Aliyun-Google"
-        url = 'https://maven.aliyun.com/repository/google'
-    }
         maven {
             //url = uri("http://sacredcraft.cn:8081/repository/releases")
-            url = uri("https://repo.spongepowered.org/maven")
+            url = uri("https://repo1.maven.org/maven2")
             //isAllowInsecureProtocol = true
            // credentials {
             //    username = project.findProperty("taboolibUsername").toString()
